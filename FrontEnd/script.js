1a3eaeda40bf628d5459518df1906d2db18ac790
Stéphanie Capiaux <stefy64@hotmail.fr>
2023-01-26 16:17:47 +0100

    Récupération de la galerie backend

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,6 +1,25 @@
-fetch ("http://localhost:5678/api/works")
-.then (res => res.json())
-.then (data => console.log(data))
+async function getAndDisplayWorks(){
+    fetch('http://localhost:5678/api/works').then(function(response) {
+      return response.json();
+  }).then(function(works) {
+    console.log(works);
+    works.forEach(elt => {
 
+        console.log(elt.imageUrl);
+      let div = document.createElement("div");
+      div.innerHTML = elt.title;
 
+      let img = document.createElement("img");
+      img.src = elt.imageUrl;
+      img.crossOrigin = "anonymous";
+      div.appendChild(img);
 
+      const sectionPortfolio = document.getElementById("portfolio");
+      sectionPortfolio.appendChild(div);
+
+    });
+  }).catch(function(err) {  
+     
+  });
+}
+getAndDisplayWorks();
