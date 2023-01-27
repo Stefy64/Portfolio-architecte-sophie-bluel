26785cf571f16a2f898210e7e3f25efe6ce3fe89
Stéphanie Capiaux <stefy64@hotmail.fr>
2023-01-27 19:25:54 +0100

    Ajout filtres dynamiques

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,16 +1,20 @@
-async function getAndDisplayWorks(){
+//Gallery
+
+async function getWorks(){
     fetch('http://localhost:5678/api/works').then(function(response) {
       return response.json();
   }).then(function(works) {
-    console.log(works);
+    console.log("photos", works);
+
     works.forEach(elt => {
 
-        console.log(elt.imageUrl);
-      let div = document.createElement("div");
-      div.innerHTML = elt.title;
-
+      
       let img = document.createElement("img");
+      let div = document.createElement("div");
+      
       img.src = elt.imageUrl;
+      div.innerHTML = elt.title;
+      
       img.crossOrigin = "anonymous";
       div.appendChild(img);
 
@@ -19,7 +23,36 @@ async function getAndDisplayWorks(){
 
     });
   }).catch(function(err) {  
-     
+    console.log("Une erreur se produit");
+    console.log(err);
   });
 }
-getAndDisplayWorks();
+getWorks();
+
+//Category
+
+async function getCategories(){
+  fetch('http://localhost:5678/api/categories').then(function(response) {
+    return response.json();
+}).then(function(categories) {
+  console.log("categories", categories);
+
+    const button = document.createElement("button");
+    const categoriesElt = document.getElementsByClassName("filters")[0];
+    button.innerHTML = "Tous";
+
+    const test = categories.map((category) => {
+    return `<button data-id="${category.id}">${category.name}</button>`
+    });
+    categoriesElt.innerHTML = test.join('');
+ 
+
+
+
+
+}).catch(function(err) {  
+  console.log("Une erreur se produit");
+  console.log(err);
+});
+}
+getCategories();
